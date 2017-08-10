7d0ab9e67c3fc1a07e4b577d719d8f7c3954c7b5
meljohndelbarrio <meljdelbarrio@yahoo.com>
2017-08-11 09:59:41 +1200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import React , { Component } from 'react';
+import './App.css';
+import {FormGroup, FormControl, InputGroup, Glyphicon} from 'react-bootstrap';
+
+class App extends Component{
+
+  constructor(props)
+  {
+    super(props);
+    this.state =
+    {
+      query: ''
+    }
+  }
+
+  search()
+  {
+    console.log(this.state);
+    const BASEURL = 'https://api.spotify.com/v1/search?';
+    const FETCHURL = `${BASEURL}q=${this.state.query}&type=artist$limit=1`;
+    console.log(FETCHURL);
+  }
+
+
+    render()
+    {
+        return(
+            <div className="App">
+
+            <div className="App-title"> Music Master</div>
+                <FormGroup>
+                    <InputGroup>
+                    <FormControl
+                        type="text"
+                        placeholder="Search for an artist"
+                        value = {this.state.query}
+                        onChange = {event => {this.setState({query:event.target.value})}}
+                        onKeyPress = {event =>
+                          {
+                            if (event.key === 'Enter') {
+                              this.search();
+                            }
+                          }
+                        }
+                  />
+
+                        <InputGroup.Addon onClick={() => this.search()} >
+                            <Glyphicon glyph="search"></Glyphicon>
+                        </InputGroup.Addon>
+                    </InputGroup>
+                </FormGroup>
+
+                <div className="Profile">
+                    <div>Artist Picture</div>
+                    <div>Artist Name</div>
+                </div>
+
+                <div className="Gallery">
+                    Gallery
+                </div>
+
+
+
+
+            </div>
+        )
+    }
+}
+
+export default App;
